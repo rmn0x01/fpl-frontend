3713fddd7e8071ea948819a48cca3a463614c190
rmn0x01 <mnizar9292@gmail.com>
2022-02-22 23:14:50 +0700

    add axios & try implementing basic get request

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,6 +3,7 @@
 </template>
 <script>
 import AuthForm from '../components/AuthForm.vue'
+import axios from 'axios'
 export default {
     name: 'Login',
     components: {
@@ -18,6 +19,13 @@ export default {
         submitLogin(email, password){
             console.log(email);
             console.log(password);
+            axios.get('http://127.0.0.1:3000/')
+                .then(response => {
+                    console.log(response.data)
+                })
+                .catch(error => {
+                    console.error(error)
+                })
         }
     }
 }
