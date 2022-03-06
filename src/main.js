c602688debfbc8addd46fa28a88ed074edc1570b
rmn0x01 <mnizar9292@gmail.com>
2022-03-06 17:37:08 +0700

    init vuex

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
+// import store from './store/store.js'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
@@ -12,5 +13,6 @@ Vue.use(IconsPlugin)
 
 new Vue({
     router,
+    // store,
     render: h => h(App)
 }).$mount('#app')
