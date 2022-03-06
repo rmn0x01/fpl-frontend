d03e065d962345142e29ad2b14dd13d61c275654
rmn0x01 <mnizar9292@gmail.com>
2022-03-06 15:55:56 +0700

    dev: implement multilevel emit for login state toggle

---
+++ src/components/LoginStateToggle.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button @click="toggleLoginState"> Login State Toggle</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginStateToggle',
+    props: {
+        isLoggedIn: Boolean
+    },
+    methods: {
+        toggleLoginState(){
+            if(this.isLoggedIn){
+                this.$emit('done-logout')
+            } else{
+                this.$emit('done-login')
+            }
+        }
+    }
+}
+</script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,18 +9,25 @@
                 <b-nav-item-dropdown text="User" right>
                     <b-dropdown-item v-if="isLoggedIn" @done-logout="logout" to="/logout">Logout</b-dropdown-item>
                     <div v-else>
-                        <b-dropdown-item @done-login="login" to="/login">Login</b-dropdown-item>
+                        <b-dropdown-item @done-login="login" to="/login">Login</b-dropdown-item> <!-- probably bug origins why isLoggedIn not updating-->
                         <b-dropdown-item to="/register">Register</b-dropdown-item>
                     </div>
                 </b-nav-item-dropdown>
+                <b-nav-item v-if="isLoggedIn"> Hello, User</b-nav-item>
+                <b-nav-item v-else> Hello, Guest </b-nav-item>
             </b-navbar-nav>
         </b-navbar>
+        <login-state-toggle  :is-logged-in="isLoggedIn" @done-login="login" @done-logout="logout"></login-state-toggle>
     </div>
 </template>
 
 <script>
+import LoginStateToggle from '@/components/LoginStateToggle.vue'
 export default {
     name: 'NavBar',
+    components: {
+        LoginStateToggle
+    },
     props: {
         isLoggedIn: Boolean
     },
